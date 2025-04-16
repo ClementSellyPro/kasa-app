61cd3a3c5f969bad6a56df94b4b1000756b16d6b
ClementSellyPro <clementselly.pro@gmail.com>
2025-04-16 17:20:48 +0400

    NotFound page done - desktop and mobile

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router";
 import styles from "./NotFound.module.scss";
 
 export default function NotFound() {
-  return <div className={styles.container}> Not Found </div>;
+  return (
+    <div className={styles.notfound}>
+      <h1 className={styles.notfound__title}>404</h1>
+      <p className={styles.notfound__text}>
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to={"/"} className={styles.notfound__link}>
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
 }
