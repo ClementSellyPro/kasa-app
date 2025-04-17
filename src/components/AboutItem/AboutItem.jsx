3346eab24df930045c9c985f28f3f3066bf1a11f
ClementSellyPro <clementselly.pro@gmail.com>
2025-04-17 15:05:08 +0400

    About page done - Fetch data and animation

--- src/components/AboutItem/AboutItem.jsx
+++ src/components/AboutItem/AboutItem.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import styles from "./AboutItem.module.scss";
 
-export default function AboutItem() {
+export default function AboutItem({ title, text }) {
   const [isOpen, setIsOpen] = useState(false);
 
   function handleClick(e) {
@@ -18,7 +18,7 @@ export default function AboutItem() {
   return (
     <div className={styles.aboutItem}>
       <div className={styles.aboutItem__header}>
-        <h2 className={styles.aboutItem__header__title}>Fiabilité</h2>
+        <h2 className={styles.aboutItem__header__title}>{title}</h2>
         <img
           className={styles.aboutItem__header__icon}
           src="/assets/icon/arrow.svg"
@@ -26,15 +26,7 @@ export default function AboutItem() {
           onClick={(e) => handleClick(e)}
         />
       </div>
-      {isOpen ? (
-        <p className={styles.aboutItem__text}>
-          Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-          photos sont conformes aux logements, et toutes les informations sont
-          régulièrement vérifiées par nos équipes.
-        </p>
-      ) : (
-        <></>
-      )}
+      {isOpen ?? <p className={styles.aboutItem__text_open}>{text}</p>}
     </div>
   );
 }
--- src/components/AboutItemList/AboutItemList.jsx
+++ src/components/AboutItemList/AboutItemList.jsx
@@ -1,13 +1,20 @@
+import { useEffect, useState } from "react";
 import AboutItem from "../AboutItem/AboutItem";
 import styles from "./AboutItemList.module.scss";
 
 export default function AboutItemList() {
+  const [aboutData, setAboutData] = useState([]);
+
+  useEffect(() => {
+    fetch("/aboutItems.json")
+      .then((response) => response.json())
+      .then((data) => setAboutData(data));
+  }, []);
   return (
     <div className={styles.aboutItemList}>
-      <AboutItem />
-      <AboutItem />
-      <AboutItem />
-      <AboutItem />
+      {aboutData.map((item) => (
+        <AboutItem title={item.title} text={item.text} />
+      ))}
     </div>
   );
 }
